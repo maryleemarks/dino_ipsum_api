8bd52926390c801d11b2e3c5ca32d1ae47d3a5c6
Mary Marks <maryleemarks@gmail.com>
2021-08-03 20:46:06 -0700

    all the things

--- src/index.js
+++ src/index.js
@@ -2,3 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+$(document).ready(function () {
+  $('#weatherLocation').click(function () {
+    let number = $('#number').val();
+    $('#number').val('');
+    $('div#links').html('');
+    // const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    let dinoUrl = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=${number}`;
+    let wikiUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${dinoName}&limit=1&namespace=0&format=json`
+
+    request.onreadystatechange = function load() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
+      }
+    };
+    request.open("GET", dinoUrl, wikiUrl, true);
+    request.send();
+
+    function forEachDinos(){
+      response[0].forEach(element => {
+        let dinoName = element;
+        load()
+        $('div#links').append(`<a href="https://en.wikipedia.org/wiki/${element}_(dinosaur)">${element}</a><br>`);
+        console.log(element)
+      });
+    }
+  });
+});
