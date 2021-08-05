0b22eb1977ff0ce450efe8b532c463a20db5f264
Mary Marks <maryleemarks@gmail.com>
2021-08-04 20:53:06 -0700

    so many more things

--- src/index.js
+++ src/index.js
@@ -7,26 +7,24 @@ $(document).ready(function () {
     let number = $('#number').val();
     $('#number').val('');
     $('div#links').html('');
-    // const city = $('#location').val();
-    $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    let dinoUrl = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=${number}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        response[0].forEach(element => {
-          $('div#links').append(`<a href="https://en.wikipedia.org/wiki/${element}">${element}</a><br>`);
-          console.log(element)
-        });
-      }
-    };
-
-
-
-    request.open("GET", dinoUrl, true);
-    request.send();
+    fetch(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=${number}`)
+    .then(response => response.json())
+    .then(data => data[0].forEach(element => {
+      fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${element}_(dinosaur)&format=json&`)
+      .then(response => response.json())
+      .then(data => {
+        let str = JSON.stringify(data[3]);//get and stringify 0th index of wiki search query
+        if(!str.includes('http')){//if the query contains the characters http
+          console.log(element+' is unique');//debug
+          // console.log('non-specific url here:' + str);//debug
+          $('div#links').append(`<a href="https://en.wikipedia.org/wiki/${element}">${element}</a><br>`);//append to DOM as specific dino type
+        } else {
+          console.log(element + " " + data[3]);
+          $('div#links').append(`<a href="${data[3]}">${element}</a><br>`);//append to DOM based on API result
+        }
+      });
+    }));
   });
+
 });
